417b789b8a688a2728bd29e8fc7bf3c4f3ed9ffc
Eric Broberg <eric@Erics-MacBook-Air.local>
2016-01-21 11:47:37 -0800

    In middle of attempt to solve countNrookssolutions

--- src/solvers.js
+++ src/solvers.js
@@ -18,34 +18,94 @@ window.findNRooksSolution = function(n) {
     'n': n
   });
   var solution = board;
+  var solutionCount = 0;
 
   var checkAndPlace = function(currentRow) {
 
     if (currentRow === n) {
+      solutionCount++;
       return;
     }
     // toggling/checking within a row
     for (var i = 0; i < board.get(0).length; i++) {
+      //create a clean board
+      // var newBoard = new Board({'n':n});
+      // checks all columns of current row.
       board.togglePiece(currentRow, i);
 
       if (board.hasRowConflictAt(currentRow) || board.hasColConflictAt(i)) {
         board.togglePiece(currentRow, i);
       }
+      //moves on to the next row
+      //clears our starting peice
+      //board.togglePiece(currentRow, i);
+      //solution = board;
     }
-    // toggling/checking the next row
     checkAndPlace(currentRow + 1);
+    // toggling/checking the next row
+   // checkAndPlace(currentRow + 1);
+   console.log(solution.rows());
   };
+   console.log("solutionCount", solutionCount);
 
   checkAndPlace(0);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution.rows();
 };
 
-
+// hasAnyRooksConflicts: function() {
+//       return this.hasAnyRowConflicts() || this.hasAnyColConflicts();
+//     },
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+  var board = new Board({'n':n});
+
+  var solutionCounter = function(currentRow, currentColumn){
+    //reset the board of n length
+    board = new Board({'n':n});
+    //base case
+    if (currentColumn === n) {
+      return;
+    }
+    if (currentRow === n) {
+      solutionCount++;
+      return;
+    }
+
+  //run find n rooks with new parameter
+    for (var i = currentColumn; i < board.get(currentRow).length; i++) {
+      //create a clean board
+      // var newBoard = new Board({'n':n});
+      // checks all columns of current row.
+        board.togglePiece(currentRow, i);
+
+      if (board.hasRowConflictAt(currentRow) || board.hasColConflictAt(i)) {
+        board.togglePiece(currentRow, i);
+      }
+      //moves on to the next row
+      //clears our starting peice
+      //board.togglePiece(currentRow, i);
+      //solution = board;
+    solutionCounter(0,currentColumn+1);  
+    }
+    solutionCounter(currentRow+1, 0);
+  //count the results of exectution
+
+  };
+  solutionCounter(0,0);
+  //start at new column for the first row with each iteration
+  //
+  // inputs: n, colIndex, rowIndex
+  // ---
+  // check for solution starting at 0,0
+  //  when found, solutionCount++
+      //recursive: make new Board | check starting at 0,1 - 0,4 |
+  //  every solution results in solutionCount++
+  // when done with row 0, start 1,0-1,4
+  // when get to end, start over at 0,0 until you get to starting place
+  // (count number squares n*n)
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
